236d9642627ff723bd0ce284cfc75177c0748df8
angelmarrugo <monoremix_@hotmail.com>
2022-01-27 07:38:23 -0500

    first commit

---
+++ lib/options.dart
@@ -0,0 +1,18 @@
+import 'package:adjoe/params.dart';
+import 'package:adjoe/user_profile.dart';
+
+/// Use this class to pass additional parameters to [Adjoe.init].
+///
+/// ```dart
+/// AdjoeOptions options = new AdjoeOptions()
+///   ..userId = 'userId'
+///   ..applicationProcessName = 'name'
+///   ..userProfile = _profile
+///   ..params = _params;
+/// ```
+class AdjoeOptions {
+  String userId;
+  String applicationProcessName;
+  AdjoeUserProfile userProfile;
+  AdjoeParams params;
+}
